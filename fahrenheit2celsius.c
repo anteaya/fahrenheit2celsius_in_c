74d4ac5e8ee84c0b464499a826f8e2822f3e0e06
Anita Kuno <ay.kuno@gmail.com>
2011-05-02 18:30:02 -0400

    now with an int variable called step

--- fahrenheit2celsius.c
+++ fahrenheit2celsius.c
@@ -13,15 +13,17 @@ int main()
 {
   float fahr, celsius;
   float lower, upper;
+  int step;
 
   lower = 0;   /* lower limit of temperature scale */
   upper = 300; /* upper limit */
+  step = step_size();
 
   fahr = lower;
   while (fahr <= upper) {
     celsius = (5.0/9.0) * (fahr-32.0);
     printf("%3.0f %6.1f\n", fahr, celsius);
-    fahr = fahr + step_size();
+    fahr = fahr + step;
   }
   return 0;
 }
