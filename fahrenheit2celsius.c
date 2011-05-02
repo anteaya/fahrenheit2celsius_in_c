d2e32fefbbd26b7fd88b2d080882be19c9660797
Anita Kuno <ay.kuno@gmail.com>
2011-05-02 18:22:14 -0400

    created a function called step_size

--- fahrenheit2celsius.c
+++ fahrenheit2celsius.c
@@ -3,20 +3,25 @@
 /* print Fahrenheit-Celsius table
    for fahr = 0, 20, ..., 300; floating-point version */
 
+/* create a function called step_size that returns 20 */
+
+int step_size() {
+  return 20;
+}
+
 int main()
 {
   float fahr, celsius;
-  float lower, upper, step;
+  float lower, upper;
 
   lower = 0;   /* lower limit of temperature scale */
   upper = 300; /* upper limit */
-  step = 20;   /* step size */
 
   fahr = lower;
   while (fahr <= upper) {
     celsius = (5.0/9.0) * (fahr-32.0);
     printf("%3.0f %6.1f\n", fahr, celsius);
-    fahr = fahr + step;
+    fahr = fahr + step_size();
   }
   return 0;
 }
