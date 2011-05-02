e1ccf47d53de6d9b8fca9da843f5b2ddcbfa9cde
Anita Kuno <ay.kuno@gmail.com>
2011-05-02 18:16:02 -0400

    now without errors

--- fahrenheit2celsius.c
+++ fahrenheit2celsius.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /* print Fahrenheit-Celsius table
-   for fahr = 0, 20, ..., 300 */
+   for fahr = 0, 20, ..., 300; floating-point version */
 
-main()
+int main()
 {
-  int fahr, celsius;
-  int lower, upper, step;
+  float fahr, celsius;
+  float lower, upper, step;
 
   lower = 0;   /* lower limit of temperature scale */
   upper = 300; /* upper limit */
@@ -14,8 +14,9 @@ main()
 
   fahr = lower;
   while (fahr <= upper) {
-    celsius = 5 * (fahr-32) / 9;
-    printf("%3d\t%6d\n", fahr, celsius);
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
+  return 0;
 }
--- floating_fahrenheit2celsius.c
+++ floating_fahrenheit2celsius.c
@@ -3,7 +3,7 @@
 /* print Fahrenheit-Celsius table
    for fahr = 0, 20, ..., 300; floating-point version */
 
-main()
+int main()
 {
   float fahr, celsius;
   float lower, upper, step;
@@ -18,4 +18,5 @@ main()
     printf("%3.0f %6.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
+	return 0;
 }
